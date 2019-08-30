fce5a883b9ddfd3ff0cfede46faec9441c5dccfb
github username <github email address>
2019-08-30 16:01:40 -0600

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
+require "pry"
 class Meal
+    attr_reader :waiter, :customer, :total, :tip
+
+    @@all = []
+    
+    def self.all
+        @@all
+    end
+
+    def initialize waiter, customer, total, tip
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        self.class.all << self
+    end
+
+
 
 end
